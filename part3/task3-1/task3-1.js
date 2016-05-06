9a4218a2fefb6f041e566ebc0c3d2f42907da4aa
jarbinup <jarbinup@gmail.com>
2016-05-06 16:42:08 +0800

    改变窗口大小未实现

--- part3/task3-1/task3-1.js
+++ part3/task3-1/task3-1.js
@@ -4,40 +4,50 @@ var getNode = function (ele) {
 };
 
 //改变遮罩状态
-function maskChange (bclname, aclname) {
+var maskChange = function(bclname, aclname) {
     var maskNode = getNode(bclname);
     maskNode.setAttribute('class', aclname);
     return maskNode;
 };
 
-实现拖拽
+//实现拖拽
 var drag = function (node) {
 	var dragNode = getNode(node);
 	//鼠标点击下时计算与client的距离
 	dragNode.onmousedown = function(event) {
-		var clientDis = {
-			offsetL: dragNode.offsetLeft,
-        	offsetT: dragNode.offsetTop,
-        	clientX: event.clientX,
-        	clientY: event.clientY
-		}
-        return clientDis;
+	    dragNode.onmousemove = function(event) {
+			var clientDis = {
+				offsetL: dragNode.offsetLeft,
+	        	offsetT: dragNode.offsetTop,
+	            clientX: event.clientX,
+	        	clientY: event.clientY
+			};
+			console.log(clientDis);
+			dragNode.style.left = clientDis.clientX +'px';
+			dragNode.style.top = clientDis.clientY +'px';
+		};
 	};
-
-	//鼠标移动时与client的距离
-	dragNode.onmousemove = function(event) {
-        
-        var dis = {
-        	disX: (moveDis.clientX >= clientDis.offsetL) ? event.clientX : clientDis.offsetL ,
-        	disY: (moveDis.clientY >= clientDis.offsetT) ? event.clientY : clientDis.offsetT
-        };
-        return dis;
+	dragNode.onmouseup = function(event) {
+		dragNode.onmousemove = null;
 	};
-
-	//设置距离
-	dragNode.style.left = dis.disX +'px';
-	dragNode.style.top = dis.disY +'px';
 }
-getNode('#mask-btn').addEventListener('click', maskChange('.mask-hidden', 'mask-show'), false);
-getNode('#quik').addEventListener('click', maskChange('.mask-show', 'mask-hidden'), false);
 
+
+getNode('#mask-btn').addEventListener('click', function(){
+	maskChange('.mask-hidden', 'mask mask-show');
+}, false);
+
+getNode('#quik').addEventListener('click', function(){
+	maskChange('.mask-show', 'mask mask-hidden');
+}, false);
+
+//阻止dialog向上冒泡
+getNode('.dialog').addEventListener('click', function(){
+	event.stopPropagation();
+}, false);
+
+getNode('.mask').addEventListener('click', function(){
+	maskChange('.mask-show', 'mask mask-hidden');
+}, false);
+
+drag('.dialog');
